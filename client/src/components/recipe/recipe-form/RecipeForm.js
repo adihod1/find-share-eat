b2e74e48863880ec80406561bd51ad986c3d8671
ADI-projects30 <adidas2712@gmail.com>
2022-07-25 00:51:58 +0300

    add recipe components

---
+++ client/src/components/recipe/recipe-form/RecipeForm.js
@@ -0,0 +1,12 @@
+import cupcakeIcon from "../../../images/cupcakeIcon.png";
+export default function RecipeForm() {
+  return (
+    <div>
+      <div className="sideTitle">findShareEat</div>
+      <div className=".app-container">
+        <FormData></FormData>
+      </div>
+      <img src={cupcakeIcon} alt="cupcake" />
+    </div>
+  );
+}
