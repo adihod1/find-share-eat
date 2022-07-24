8d1a87f3736700cecceb3792f4487bc79717b51f
Alex Levenberg <zeronid@gmail.com>
2022-07-24 16:39:15 +0300

    ADD: sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from './components/pages/main/main-component'
+import Sidebar from "./components/sidebar/Sidebar";
+import Profile from "./components/pages/profile/Profile";
 
 function App() {
   return (
     <BrowserRouter>
+      <Sidebar />
       <Routes>
         <Route index element={<Main />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/saved" element={<Main />} />
+        <Route path="/shared" element={<Main />} />
       </Routes>
     </BrowserRouter>
   );
