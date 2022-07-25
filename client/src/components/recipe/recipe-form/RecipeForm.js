c246f1cb6199ebc547b808f766903e71133b205c
ADI-projects30 <adidas2712@gmail.com>
2022-07-25 13:08:50 +0300

    add part of the front

--- client/src/components/recipe/recipe-form/RecipeForm.js
+++ client/src/components/recipe/recipe-form/RecipeForm.js
@@ -1,12 +1,29 @@
 import cupcakeIcon from "../../../images/cupcakeIcon.png";
+import findShareEatLogo from "../../../images/findShareEatLogo.png";
+import penguinIcon from "../../../images/penguinIcon.png";
+
+import "./RecipeForm.scss";
+
 export default function RecipeForm() {
   return (
-    <div>
-      <div className="sideTitle">findShareEat</div>
-      <div className=".app-container">
-        <FormData></FormData>
+    <div className="container">
+      <div className="sideTitle">
+        {/* <link
+          href="https://fonts.googleapis.com/css?family=Potta One"
+          rel="stylesheet"
+        /> */}
+        <img className="logo-img" src={findShareEatLogo} alt="logo" />
+        <h1 className="font">TIME</h1>
+        <h1 className="font">TO</h1>
+        <h1 className="font">COOK</h1>
+        <img className="penguin-img" src={penguinIcon} alt="logo" />
       </div>
-      <img src={cupcakeIcon} alt="cupcake" />
+      <div className="app-container">
+        <form>hey</form>
+      </div>
+      <div className="cupcake-img">
+        <img src={cupcakeIcon} alt="cupcake" />
+      </div>
     </div>
   );
 }
