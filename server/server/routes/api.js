ba74c1b61807f4047f880d8322a446dd6f2da0c3
ADI-projects30 <adidas2712@gmail.com>
2022-07-28 22:50:55 +0300

    add routing

--- server/server/routes/api.js
+++ server/server/routes/api.js
@@ -2,16 +2,66 @@ const express = require("express");
 const router = express.Router();
 const itemManager = require("../services/item-manager");
 
-router.get("/items", async (_, res) => {
-  res.status(200).json(await new itemManager().createItems());
+router.post("/add-user", async (_, res) => {
+  res.status(200).json(await new itemManager().addUser());
 });
 
-router.get("/recipes", async (_, res) => {
-  res.status(200).json(await new itemManager().createRecipe());
+router.post("/add-recipe", async (_, res) => {
+  res.status(200).json(await new itemManager().addRecipe());
+});
+
+router.get("/recipes-all", async (_, res) => {
+  res.status(200).json(await new itemManager().getAllRecipes());
+});
+
+router.get("/ingredients-by-recipe/:id", async (_, res) => {
+  res.status(200).json(await new itemManager().getIngredientsByRecipe(3));
+});
+
+router.get("/recipe-by-user", async (_, res) => {
+  res.status(200).json(await new itemManager().getRecipeByUser(1));
+});
+
+router.get("/chosen-recipe", async (_, res) => {
+  res.status(200).json(await new itemManager().getChosenRecipe(1));
+});
+
+router.get("/recipes-by-category", async (_, res) => {
+  res.status(200).json(await new itemManager().getRecipeByCategory("Dessert"));
+});
+
+router.get("/recipes-by-ingredient", async (_, res) => {
+  res
+    .status(200)
+    .json(await new itemManager().getRecipeByIngredients(["salt", "sugar"]));
+});
+
+router.get("/recipes-by-name", async (_, res) => {
+  res.status(200).json(await new itemManager().getRecipeByName("wo"));
+});
+
+router.get("/get-user", async (_, res) => {
+  res.status(200).json(await new itemManager().getUser(1));
+});
+
+router.get("/get-categories", async (_, res) => {
+  res.status(200).json(await new itemManager().getCategories());
 });
 
 router.get("/ingredients", async (_, res) => {
   res.status(200).json(await new itemManager().createIngredients());
+});
+
+router.put("/edit-recipe", async (_, res) => {
+  res.status(200).json(await new itemManager().editRecipe(9));
+});
+
+router.put("/edit-ingredient", async (_, res) => {
+  res.status(200).json(await new itemManager().editIngredient(2));
+});
+
+router.delete("/delete-recipe", async (_, res) => {
+  res.status(200).json(await new itemManager().deleteRecipe(6));
 });
 
 module.exports = router;
--- server/server/services/item-manager.js
+++ server/server/services/item-manager.js
@@ -1,28 +1,67 @@
-const { User, Recipe, Ingredient } = require("../db/models");
+const {
+  User,
+  Recipe,
+  Ingredient,
+  UsersRecipes,
+  Category,
+} = require("../db/models");
+
+const Op = require("Sequelize").Op;
 
 class itemManager {
-  // createItems = async () => {
-  //   User.create(
-  //     {
-  //       firstName: "heeyy",
-  //       lastName: "heeyy",
-  //       userName: "hewyyeye",
-  //       email: "heyewyey@gmail.com",
-  //       password: "12345",
-  //       Recipes: [
-  //         {
-  //           recipeName: "cake",
-  //           description: "cake",
-  //           cookingTime: "1:00",
-  //           instructions: "buy cake",
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       include: [Recipe],
-  //     }
-  //   );
-  // };
+  createItems = async () => {
+    User.create(
+      {
+        firstName: "heeyy",
+        lastName: "heeyy",
+        userName: "hewtyyeye",
+        email: "heyewytey@gmail.com",
+        password: "12345",
+        Recipes: [
+          {
+            recipeName: "cake",
+            description: "cake",
+            cookingTime: "1:00",
+            instructions: "buy cake",
+          },
+        ],
+      },
+      {
+        include: [Recipe],
+      }
+    );
+  };
+
+  addRecipe = async () => {
+    Recipe.create(
+      {
+        recipeName: "cakke",
+        description: "cakke",
+        cookingTime: "1:00",
+        instructions: "buy cake",
+        Ingredients: [
+          {
+            ingredientName: "sugar",
+            amount: 300,
+            measurement: "1 cup",
+          },
+          {
+            ingredientName: "salt",
+            amount: 300,
+            measurement: "1 cup",
+          },
+          {
+            ingredientName: "water",
+            amount: 300,
+            measurement: "1 cup",
+          },
+        ],
+      },
+      {
+        include: [Ingredient],
+      }
+    );
+  };
   // createRecipe = async () => {
   //   Recipe.create({
   //     recipeName: "cake",
@@ -39,6 +78,97 @@ class itemManager {
   //     recipeId: "1",
   //   });
   // };
+
+  addUser = async () => {
+    await User.create({
+      firstName: "adiii",
+      lastName: "hoddd",
+      userName: "adihoddd",
+      email: "adihoddd@gmail.com",
+      password: "125345",
+    });
+  };
+
+  getAllRecipes = async () => {
+    const recipes = await Recipe.findAll({
+      include: { model: Ingredient },
+    });
+    return recipes;
+  };
+
+  getRecipeByUser = async (id) => {
+    const recipes = await User.findByPk(id, {
+      include: [{ model: Recipe, include: { model: Ingredient } }],
+    });
+    return recipes;
+  };
+
+  getChosenRecipe = async (id) => {
+    const recipe = await Recipe.findByPk(id, {
+      include: { model: Ingredient },
+    });
+    return recipe;
+  };
+
+  getRecipeByCategory = async (category) => {
+    const recipe = await Category.findAll({
+      include: [{ model: Recipe, include: { model: Ingredient } }],
+      where: { categoryName: category },
+    });
+    return recipe;
+  };
+
+  getRecipeByRecipeName = async (ingredients) => {
+    const ingredientsAll = ingredients.map((ingredient) => {
+      return { ingredientName: ingredient };
+    });
+    const recipe = await Ingredient.findAll({
+      include: [{ model: Recipe, include: { model: Ingredient } }],
+      where: {
+        [Op.or]: ingredientsAll,
+      },
+    });
+    return recipe;
+  };
+
+  getRecipeByName = async (search) => {
+    const recipe = await Recipe.findAll({
+      include: [{ model: Ingredient }],
+      where: { [Op.and]: [{ recipeName: { [Op.like]: `%${search}%` } }] },
+    });
+    return recipe;
+  };
+
+  getUser = async (id) => {
+    return await User.findByPk(id);
+  };
+
+  getCategories = async () => {
+    return await Category.findAll();
+  };
+
+  editRecipe = async (id) => {
+    await Recipe.update(
+      { recipeName: "chocolate cake" },
+      { where: { id: id } }
+    );
+  };
+
+  editIngredient = async (id) => {
+    await Ingredient.update(
+      { ingredientName: "flower" },
+      { where: { id: id } }
+    );
+  };
+
+  deleteRecipe = async (id) => {
+    await Recipe.destroy({
+      where: {
+        id: id,
+      },
+      include: [{ model: Ingredient, include: { where: { recipeId: id } } }],
+    });
+  };
 }
 
 module.exports = itemManager;
