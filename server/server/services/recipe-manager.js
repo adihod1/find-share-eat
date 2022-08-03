2eea4dcd9e03ae6d980f390b8f1a4290fc3d47d5
ADI-projects30 <adidas2712@gmail.com>
2022-08-03 23:30:57 +0300

    add file dont change

--- server/server/services/recipe-manager.js
+++ server/server/services/recipe-manager.js
@@ -1,92 +1,106 @@
 const {User, Recipe, Ingredient, UsersRecipes, Category, sequelize} = require("../db/models");
+const {
+  Recipe,
+  Ingredient,
+  UsersRecipes,
+  Category,
+  sequelize,
+} = require("../db/models");
 
 const Op = require("Sequelize").Op;
 
 class RecipeManager {
-    addRecipe = async (id, data) => {
-        const recipe = await Recipe.create({
-                ...data.recipe,
-                Ingredients: data.ingredients,
-            },
-            {include: [Ingredient],}
+  addRecipe = async (id, data) => {
+    const recipe = await Recipe.create(
+      {
+        ...data.recipe,
+        Ingredients: data.ingredients,
+      },
+      { include: [Ingredient] }
+    );
+    const userRow = await User.findByPk(id);
+    await recipe.addUser(userRow, { through: UsersRecipes });
+  };
+
+  getAllRecipes = async () => {
+    const recipes = await Recipe.findAll({
+      include: { model: Ingredient },
+    });
+    return recipes;
+  };
+
+  getRecipeByUser = async (id) => {
+    const recipes = await User.findByPk(id, {
+      include: [{ model: Recipe, include: { model: Ingredient } }],
+    });
+    return recipes;
+  };
+
+  getChosenRecipe = async (id) => {
+    const recipe = await Recipe.findByPk(id, {
+      include: { model: Ingredient },
+    });
+    return recipe;
+  };
+
+  getRecipeByCategory = async (category) => {
+    const recipe = await Category.findAll({
+      include: [{ model: Recipe, include: { model: Ingredient } }],
+      where: { categoryName: category },
+    });
+    return recipe;
+  };
+
+  getRecipeByIngredients = async (ingredients) => {
+    const recipe = await Ingredient.findAll({
+      include: [{ model: Recipe, include: { model: Ingredient } }],
+      where: {
+        [Op.or]: ingredients,
+      },
+    });
+    return recipe;
+  };
+
+  getRecipeByName = async (search) => {
+    const recipe = await Recipe.findAll({
+      include: [{ model: Ingredient }],
+      where: { [Op.and]: [{ recipeName: { [Op.like]: `%${search}%` } }] },
+    });
+    return recipe;
+  };
+
+  editRecipe = async (id, data) => {
+    await Recipe.update(data.recipe, { where: { id: id } });
+  };
+
+  deleteRecipe = async (id) => {
+    try {
+      const result = await sequelize.transaction(async (t) => {
+        await UsersRecipes.destroy(
+          {
+            where: { recipeId: id },
+          },
+          { transaction: t }
         );
-        const userRow = await User.findByPk(id);
-        await recipe.addUser(userRow, {through: UsersRecipes});
-    };
-
-    getAllRecipes = async () => {
-        const recipes = await Recipe.findAll({
-            include: {model: Ingredient},
-        });
-        return recipes;
-    };
-
-    getRecipeByUser = async (id) => {
-        const recipes = await User.findByPk(id, {
-            include: [{model: Recipe, include: {model: Ingredient}}],
-        });
-        return recipes;
-    };
-
-    getChosenRecipe = async (id) => {
-        const recipe = await Recipe.findByPk(id, {
-            include: {model: Ingredient},
-        });
-        return recipe;
-    };
-
-    getRecipeByCategory = async (category) => {
-        const recipe = await Category.findAll({
-            include: [{model: Recipe, include: {model: Ingredient}}],
-            where: {categoryName: category},
-        });
-        return recipe;
-    };
-
-    getRecipeByIngredients = async (ingredients) => {
-        const recipe = await Ingredient.findAll({
-            include: [{model: Recipe, include: {model: Ingredient}}],
-            where: {
-                [Op.or]: ingredients,
-            },
-        });
-        return recipe;
-    };
-
-    getRecipeByName = async (search) => {
-        const recipe = await Recipe.findAll({
-            include: [{model: Ingredient}],
-            where: {[Op.and]: [{recipeName: {[Op.like]: `%${search}%`}}]},
-        });
-        return recipe;
-    };
-
-    editRecipe = async (id, data) => {
-        await Recipe.update(data.recipe, {where: {id: id}});
-    };
-
-    deleteRecipe = async (id) => {
-        try {
-            const result = await sequelize.transaction(async (t) => {
-                await UsersRecipes.destroy({
-                        where: {recipeId: id},
-                    }, {transaction: t}
-                );
-                await Recipe.destroy({
-                        where: {id: id,},
-                    }, {transaction: t}
-                );
-                await Ingredient.destroy({
-                        where: {recipeId: id},
-                    }, {transaction: t}
-                );
-            });
-            return true;
-        } catch (e) {
-            console.log(e);
-            return false;
-        }
-    };
+        await Recipe.destroy(
+          {
+            where: { id: id },
+          },
+          { transaction: t }
+        );
+        await Ingredient.destroy(
+          {
+            where: { recipeId: id },
+          },
+          { transaction: t }
+        );
+      });
+      return true;
+    } catch (e) {
+      console.log(e);
+      return false;
+    }
+  };
 }
 
 module.exports = new RecipeManager();
